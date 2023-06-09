35d116332302719500fedd64fa70d723e9ee5c09
João Paulo Ferreira <paulojp.dev@gmail.com>
2023-06-09 18:38:14 -0300

    Persist quotation with 10ms for limit time

--- server.go
+++ server.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"time"
 
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/tidwall/gjson"
 )
 
 func main() {
-	http.HandleFunc("/cotacao", GetDolarCotationHandler)
+	http.HandleFunc("/cotacao", GetDollarQuotationHandler)
 	http.ListenAndServe(":8080", nil)
-
-	// TODO: 10ms máximo para salvar no banco de dados SQLite
 }
 
-func GetDolarCotationHandler(response http.ResponseWriter, request *http.Request) {
-	cotation, error := GetDolarCotation()
+func GetDollarQuotationHandler(response http.ResponseWriter, request *http.Request) {
+	quotation, error := GetDollarQuotation()
 	if error != nil {
 		response.WriteHeader(http.StatusInternalServerError)
 		result := map[string]string{"error": error.Error()}
@@ -27,11 +27,11 @@ func GetDolarCotationHandler(response http.ResponseWriter, request *http.Request
 	}
 	response.Header().Set("Content-Type", "application/json")
 	response.WriteHeader(http.StatusOK)
-	result := map[string]float64{"value": cotation}
+	result := map[string]float64{"value": quotation}
 	json.NewEncoder(response).Encode(result)
 }
 
-func GetDolarCotation() (float64, error) {
+func GetDollarQuotation() (float64, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
 	defer cancel()
 	request, error := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
@@ -51,5 +51,49 @@ func GetDolarCotation() (float64, error) {
 	if error != nil {
 		return 0, error
 	}
+	error = persistQuotation(result.Float())
+	if error != nil {
+		return 0, error
+	}
 	return result.Float(), nil
+}
+
+func persistQuotation(quotation float64) error {
+	db, error := prepareDB()
+	if error != nil {
+		return error
+	}
+	defer db.Close()
+	stmt, error := db.Prepare("insert into cotations values(NULL, 'USD', $1, $2)")
+	if error != nil {
+		return error
+	}
+	defer stmt.Close()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancel()
+	_, error = stmt.ExecContext(ctx, quotation, time.Now())
+	if error != nil {
+		return error
+	}
+	return nil
+}
+
+func prepareDB() (*sql.DB, error) {
+	db, error := sql.Open("sqlite3", "serverDB.db")
+	if error != nil {
+		return nil, error
+	}
+	createTable := `
+		CREATE TABLE IF NOT EXISTS cotations (
+			id INTEGER NOT NULL PRIMARY KEY,
+			currency TEXT NOT NULL,
+			value REAL NOT NULL,
+			time DATETIME NOT NULL
+		);
+	`
+	_, error = db.Exec(createTable)
+	if error != nil {
+		return nil, error
+	}
+	return db, nil
 }
